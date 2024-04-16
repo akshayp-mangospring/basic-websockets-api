eccc14bdbd307c08e859c690809c50b700f794a6
akshayp-mangospring <akshayp@mangospring.com>
2024-04-16 21:43:12 +0530

    Add functionality to Convert `.pdf` to `.docx` for my Prajakta

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const envConfig = require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const pythonShell = require('python-shell').PythonShell;
 const express = require('express');
 const cors = require('cors');
 const { createServer } = require('node:http');
@@ -9,6 +13,20 @@ const {
   updateTodoFromTodoList
 } = require('./controllers/todo_controller');
 const { getAllTodolists, createTodoList, deleteTodoList } = require('./controllers/todolist_controller');
+const { PythonShell } = require('python-shell');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+});
+
+const upload = multer({
+  storage,
+})
 
 const { parsed: { PORT } } = envConfig;
 const app = express();
@@ -18,8 +36,39 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send({
-    content: 'Hello, World!'
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/pdftodocx', upload.single('pdf'), (req, res) => {
+  if (!req.file) {
+    res.status(400).send('No file uplaoded');
+    return;
+  }
+
+  const pyShell = new PythonShell('/convert_pdf_to_docx.py', {
+    mode: 'text',
+    pythonPath: 'python3',
+    scriptPath: __dirname,
+    args: [req.file.path]
+  });
+
+  pyShell.on('message', (msg) => {
+    console.log(msg);
+  });
+
+  pyShell.end((err) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+
+    const docxFile = req.file.path.replace('.pdf', '.docx');
+    res.download(docxFile, 'converted.docx');
+  });
+
+  pyShell.on('error', (msg) => {
+    console.log(msg);
+    res.status(500).send('An error occurred');
   });
 });
 
